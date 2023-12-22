4bf8f5b5e4e039df8085d2853650a63102a852a8
Preety001 <89682311+Preety001@users.noreply.github.com>
2023-12-22 12:53:49 +0530

    Update iFalcon.java

--- iFalcon.java
+++ iFalcon.java
@@ -1,27 +1,49 @@
+// Code with the main function 
+
+// * Make a file: ParenthesesValidation.java
+// * Paste the following code in it.
+// * To test the code kindly read the README.md file.
+
+import java.util.Scanner;
 import java.util.Stack;
 
-public class Assignment {
-    public boolean isValid(String s) {
+public class ParenthesesValidation {
+    public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
         for (char ch : s.toCharArray()) {
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
-            } 
-            else {
-                if (stack.empty()) {
+            } else {
+                if (stack.empty())
                     return false;
-                }
-
-                if ((ch == ')' && stack.peek() == '(') || (ch == '}' && stack.peek() == '{') || (ch == ']' && stack.peek() == '[')) {
+                if ((stack.peek() == '(' && ch == ')') || (stack.peek() == '{' && ch == '}') || (stack.peek() == '[' && ch == ']')) {
                     stack.pop();
-                } 
-                else {
+                } else
                     return false;
-                }
+            }
+        }
+        return stack.empty();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of test cases you want to test: ");
+        int numberOfTestcases = scanner.nextInt();
+
+        while (numberOfTestcases-- > 0) {
+            System.out.print("Enter the input string: ");
+            String inputString = scanner.next();
+
+            if (isValid(inputString)) {
+                System.out.println("The input string is valid");
+            } else {
+                System.out.println("The input string is invalid");
             }
         }
 
-        return stack.empty();
+        scanner.close();
     }
 }
+
+
