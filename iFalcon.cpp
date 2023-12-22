523cb3b8541fea8da656b5a3d0445063ac335ca2
Preety001 <89682311+Preety001@users.noreply.github.com>
2023-12-22 12:37:21 +0530

    Update iFalcon.cpp

--- iFalcon.cpp
+++ iFalcon.cpp
@@ -24,3 +24,60 @@ public:
         return Stack.empty();
     }
 };
+
+
+// The code with main function
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s)
+{
+    stack<char> Stack;
+    for (auto &ch : s)
+    {
+        if (ch == '(' or ch == '{' or ch == '[')
+        {
+            Stack.push(ch);
+        }
+
+        else
+        {
+            if (Stack.empty())
+                return false;
+            if ((Stack.top() == '(' and ch == ')') or (Stack.top() == '{' and ch == '}') or (Stack.top() == '[' and ch == ']'))
+            {
+                Stack.pop();
+            }
+            else
+                return false;
+        }
+    }
+
+    return Stack.empty();
+}
+
+int main()
+{
+    int numberOfTestcases;
+    cout<<"Enter the number of testcases you want to test: ";
+    cin >> numberOfTestcases;
+
+    while (numberOfTestcases--)
+    {
+        string s;
+        cout << "Enter the input string: ";
+        cin >> s;
+
+        if (isValid(s))
+        {
+            cout << "The input string is valid" << endl;
+        }
+        else
+        {
+            cout << "The input string is invalid" << endl;
+        }
+    }
+    return 0;
+}
+
